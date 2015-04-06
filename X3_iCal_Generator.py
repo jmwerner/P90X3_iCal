938e3b27694a4cc8970ca5b7ee20c8db155ebc58
Jeremy Werner <jeremy@jerwerner.com>
2015-04-05 17:06:25 -0700

    Minor misc changes, fixed mistakes from previous order write-down, added print calendar function to check for errors as well

--- X3_iCal_Generator.py
+++ X3_iCal_Generator.py
@@ -3,25 +3,38 @@
 
 from icalendar import Calendar, Event
 from datetime import datetime, timedelta
-import pytz, tempfile, os
+from numpy import reshape
+import pytz
+import tempfile
+import os
+
 
 ######
 ######
 # Start Date - year, month, day, hour, minute, second
 # Coded right now to last one hour and be added to calendar
 #  at the same time every day until finished
-start_date = datetime(2015, 1, 11, 6, 0, 0)
+start_date = datetime(2015, 4, 11, 5, 30, 0)
 # Output directory
-directory = '/your/directory/here'
+directory = '/Users/jmwerner1123/Desktop'
 ######
 ######
+
+
+# Function to print a rough view of the calendar to check if it's correct
+def printcal(names, ord):
+	n_weeks = int(len(ord) / 7) + 1
+	mat = [0 for x in range(n_weeks * 7)]
+	for i in range(len(ord)):
+		mat[i] = names[ord[i] - 1]
+	print(reshape(mat, (n_weeks,7)))
 
 # Hard coded order of workouts (not the most efficient way, but it works!)
 
-# This is the order for the "classic calendar"  http://workoutscheduleking.com/wp-content/uploads/2013/12/P90X3_Classic_Schedule.png
-order = [1,2,3,4,15,6,7, 
-1,2,3,4,15,6,7,
-1,2,3,4,15,6,7,
+# This is the order for the "classic calendar"  http://rippedclub.net/wp-content/uploads/2013/12/P90X3-Classic-Calendar.png
+order = [1,2,3,4,15,9,7, 
+1,2,3,4,15,9,7,
+1,2,3,4,15,9,7,
 8,7,16,5,15,3,7,
 10,17,3,11,6,12,7,
 10,17,3,11,6,12,7,
@@ -33,21 +46,20 @@ order = [1,2,3,4,15,6,7,
 13,12,10,17,5,11,7,
 8,16,5,3,7,7]
 
-# My guess/ kind of following this website's suggestion:  http://tylerrobbinsfitness.com/blog/2014/2/8/when-should-you-add-ab-ripper-xx2x3-into-the-p90x3-schedule
-# Change to fit your workout needs
-ab_ripper = [0,0,1,0,1,0,0,
-0,0,1,0,1,0,0,
-0,0,1,0,1,0,0,
-0,0,0,0,0,1,0,
-0,0,1,0,0,1,0,
-0,0,1,0,0,1,0,
-0,0,1,0,0,1,0,
-0,0,0,0,0,1,0,
-0,0,1,0,0,1,0,
-0,1,0,0,0,0,0,
-0,0,1,0,0,1,0,
-0,1,0,0,0,0,0,
-0,0,0,1,0,0]
+# New schedule for Ab Ripper (corresponding to classical calendar)
+ab_ripper = [0,0,0,1,0,0,1,
+0,0,0,1,0,0,1,
+0,0,0,1,0,0,1,
+0,1,0,0,0,0,1,
+0,0,1,0,0,0,1,
+0,0,1,0,0,0,1,
+0,0,1,0,0,0,1,
+0,1,0,0,0,0,1,
+0,0,1,0,0,0,1,
+0,0,1,0,0,0,1,
+0,0,1,0,0,0,1,
+0,0,1,0,0,0,1,
+0,0,0,0,1,0]
 
 # This is the order for the "mass calendar"
 # order = [1,2,3,4,5,6,7, 
@@ -68,18 +80,19 @@ ab_ripper = [0,0,1,0,1,0,0,
 # order = [16,9,3,15,8,2,7, 
 # 16,9,3,15,8,2,7,
 # 16,9,3,15,8,2,7,
-# 6,7,16,5,2,3,7,
+# 8,7,16,5,2,3,7,
 # 17,9,3,12,6,15,7,
 # 17,9,3,12,6,15,7,
 # 17,9,3,12,6,15,7,
-# 6,7,16,5,2,3,7,
+# 8,7,16,5,15,3,7,
 # 13,12,11,3,17,10,7,
 # 12,13,17,5,13,15,7,
 # 13,12,11,3,17,10,7,
 # 12,13,17,5,13,15,7,
 # 8,16,5,3,7,7]
 
-
+# No Ab Ripper
+# ab_ripper = [0 for i in range(90)]
 
 
 # Names of workouts
@@ -100,6 +113,7 @@ for x in range(0,len(order)-1,1):
 	event.add('dtstart', newday)
 	event.add('dtend', newday + timedelta(hours=1))
 	cal.add_component(event)
+	# ab_ripper[x] ... hehe
 	if ab_ripper[x]:
 		event = Event()
 		event.add('summary', 'Ab Ripper X3')
